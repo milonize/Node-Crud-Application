2a3fd483ce9407eb2a5154787f0323a5840862c5
Coding House <coding.house240@gmail.com>
2024-04-23 16:29:07 +0600

    connect to the database by using mongoose

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express=require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app=express();
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
+mongoose.connect('mongodb://localhost/todosApplication')
+  .then(() => console.log('connected')) // Corrected line: chain .then() after connect()
+  .catch(err => console.log(err));
+
+app.listen(3000, () => {
+    console.log('connection success on port 3000');
+});
