5d878aae89eb00543d0235669e4ce7522dc5e2f5
Coding House <coding.house240@gmail.com>
2024-04-28 22:49:21 +0600

    Project successfully completed
    Now i am make this code better optimized and find any bug in this project.

--- crudRouteHandler/crudRouteHandler.js
+++ crudRouteHandler/crudRouteHandler.js
@@ -8,37 +8,108 @@ const router= express.Router()
 
 //Read all Data
 router.get('/read-all',async(req,res)=>{
-    res.send('read all data');
+    try {
+        const data = await Crud.find({}).select({
+            _id:0,
+            __v:0
+        }).limit(2);
+        res.status(200).json({ result: data });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
 //Read one Data
 router.get('/read/:id',async(req,res)=>{
-    res.send('read by id');
+    try {
+        const data = await Crud.find({_id:req.params.id}).select({
+            _id:0,
+            __v:0
+        });
+        res.status(200).json({ result: data });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
 
-//Post Data
+//create one data in to the database
 router.post('/create',async(req,res)=>{
 
 const newCrud= new Crud(req.body)
-// Inside your route handler where you're saving a document
 try {
-    // Assuming "document" is an instance of a Mongoose model
     const result = await newCrud.save();
-    // Handle success - "result" contains the saved document
-    res.send(result);
+    res.status(200).send(result);
   } catch (error) {
     // Handle error
     res.status(500).send(error.message);
   }
   
+})
 
+//create many data in to the database
+router.post('/create-many',async(req,res)=>{
+    try{
+       await Crud.insertMany(req.body)
+       res.status(200).send('success to upload many data');
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+   
 })
-//put Data
+
+
+//Update one Data
 router.put('/update/:id',async(req,res)=>{
-    res.send('update by id');
+try{
+    await Crud.updateOne({_id:req.params.id},{
+        $set:{
+            status:'inactive'
+        }
+    })
+    res.status(200).send('Success to update record');
+
+}catch(error){
+    res.status(500).send(error.message);
+
+}
 })
+
+//Update many Data
+router.put('/update-all',async(req,res)=>{
+    try{
+        await Crud.updateMany({status:'inactive'},{
+            $set:{
+                status:'active'
+            }
+        })
+        res.send('Success to update many record');
+    
+    }catch(error){
+        res.status(500).send(error.message);
+    
+    }
+    })
 //deleted Data
 router.delete('/delete/:id',async(req,res)=>{
-    res.send('deleted by id');
+  try{
+    await Crud.deleteOne({_id:req.params.id})
+    res.status(200).send('Success to delete data')
+  }catch(error){
+    res.status(500).send('Failed to deleted data')
+
+  }
 })
+
+
+//deleted Data
+router.delete('/delete-all/',async(req,res)=>{
+    try{
+      await Crud.deleteMany({status:'inactive'})
+      res.status(200).send('Success to delete all data')
+    }catch(error){
+      res.status(500).send('Failed to deleted all data')
+  
+    }
+  })
+
 module.exports=router;
